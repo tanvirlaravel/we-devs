cdfae0e8ab46566906c188f22da7d1c0e8d671a0
Tanvir <tanvir.sajib.programmer@gmail.com>
2024-05-11 15:57:16 +0600

    Wedevs top menu added

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -19,8 +19,20 @@ class Menu {
 	
 	
 	public function admin_menu(){
-		echo 'admin menu';
-		exit;
+		/**
+		* The add_menu_page() function 
+		* in WordPress is used to add a top-level menu item to the WordPress admin menu. This function 
+		* is typically called within the admin_menu action hook to register custom menu items.
+		*
+		* add_menu_page(string $page_title,	string $menu_title,string $capability,string $menu_slug,
+		*				callable $function = '',string $icon_url = '',int $position = null );
+		*/
+		add_menu_page('Wedevs academy', 'wedevs academy', 'manage_options', 'wedevs-academy', [$this, 'plugin_page'], 'dashicons-welcome-learn-more');
+	}
+	
+	
+	public function plugin_page(){
+		echo "Plugin page content";
 	}
 	
 	
