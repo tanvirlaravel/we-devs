6aeb7629f34c9afe2d05ffa8d28243a2556cd7da
Tanvir <tanvir.sajib.programmer@gmail.com>
2024-05-11 12:34:59 +0600

    plugin main class cateted and initiated

--- we-devs.php
+++ we-devs.php
@@ -3,4 +3,37 @@
 * Plugin Name: WeDevs Plugin
 */
 
-echo 'hi';
+ /**
+	* defined() 
+	* defined() function in PHP is used to check whether a constant has been defined or not. 
+	* Constants are values that cannot be changed during the execution of a script, and they 
+	* are typically defined using the define() function in PHP.
+	*	
+	* @return true/false
+ */
+/**
+   * ABSPATH
+   * ABSPATH is a constant that represents the absolute path to the WordPress installation directory.
+ */
+ if( ! defined( 'ABSPATH' ) ){ 
+    exit;
+ }
+ 
+  /**
+  * The main class of wedevs plugin.
+  */
+  
+class We_Devs {
+	public function __construct(){
+		
+	}  
+}
+
+
+
+  
+function we_devs(){
+	  new We_Devs();
+}
+
+we_devs();
